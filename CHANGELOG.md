0d5ad2240be96a2e7725755f55e310456f40eb5f
Gabriel Unterholzer <unterholzer.gabriel@gmail.com>
2021-03-15 16:51:20 +0100

    Fixed error due to JsSIP version in user agent

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,7 +5,11 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
  
 ## [Unreleased]
- 
+
+## [0.11.1] - 2021-03-15
+### Fixed
+- JsSIP version in user agent 
+
 ## [0.11.0] - 2021-03-15
 ### Removed
 - Legacy PIDF-LO support for DEC112 environments
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,5 @@
-import { getDependencyInfo, getPackageInfo } from "../utils/package-utils"
+import { getPackageInfo } from "../utils/package-utils"
 
 const packageInfo = getPackageInfo();
-const jssipVersion = getDependencyInfo('jssip')?.version ?? 'unknown';
 
-export const USER_AGENT = `${packageInfo.name} ${packageInfo.version} JsSIP ${jssipVersion}`;
+export const USER_AGENT = `${packageInfo.name} ${packageInfo.version} with JsSIP`;
