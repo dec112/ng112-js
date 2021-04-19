69589084adea92a017a896f296b9005e4fcefbe5
Gabriel Unterholzer <unterholzer.gabriel@gmail.com>
2021-04-19 15:36:07 +0000

    Include JsSIP version in UserAgent

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
  
 ## [Unreleased]
 
+## [0.11.19] - 2021-04-19
+### Added
+- JsSIP version in UserAgent string
+
 ## [0.11.18] - 2021-04-18
 ### Fixed
 - Fixed heartbeat for continued conversations
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,6 @@
 import { getPackageInfo } from "../utils/package-utils"
+import { version as jssipVersion } from "jssip";
 
 const packageInfo = getPackageInfo();
 
-export const USER_AGENT = `${packageInfo.name} ${packageInfo.version} with JsSIP`;
+export const USER_AGENT = `${packageInfo.name} ${packageInfo.version}, JsSIP ${jssipVersion}`;
