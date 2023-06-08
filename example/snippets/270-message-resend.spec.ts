2613fd9d774dd9a9e2fec8c65b25fa6efd465601
Gabriel Unterholzer <unterholzer.gabriel@gmail.com>
2023-06-08 19:02:44 +0200

    Improved resend example

--- example/snippets/270-message-resend.spec.ts
+++ example/snippets/270-message-resend.spec.ts
@@ -2,17 +2,19 @@ import { createAgent, endExample, startExample } from './util';
 
 it('Shows how to resend a message', async () => {
   startExample();
-  
+
   const agent = await createAgent();
 
   const conversation = agent.createConversation('sip:144@dec112.eu');
 
   // should be awaited!
-  const startMsg = conversation.start();
-  startMsg.promise.catch(() => {
-    const newStartMsg = startMsg.resend();
-
-    newStartMsg.promise.then(() => console.log('this is how resend works'));
+  const firstStartMsg = conversation.start();
+  firstStartMsg.promise.catch(() => {
+    // here we can resend the message
+    // should be awaited!
+    // IMPORTANT: to not cause any id collisions this message is
+    // a NEW object with a NEW message id!
+    const secondStartMsg = firstStartMsg.resend();
   });
 
   endExample();
